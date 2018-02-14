52c1f274ef2908331b9ff9114d4c73cb6925e8cf
evgeshakrasava <zplacesound@gmail.com>
2018-02-14 18:03:49 +0300

    added servo

--- tom-vs-s-master/fireball.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-
-class Fireball():
-    def __init__(self):
-        self.screen = screen
-        self.image =pygame.image.load("images/fireball.bmp")
-        self.rect = self.image.get_rect()
-        self.screen_rect = screen_
