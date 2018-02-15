8e8c20a4d31fa4a91a91b21e75a328aeea302894
evgeshakrasava <zplacesound@gmail.com>
2018-02-15 18:44:04 +0300

    Add files via upload

---
+++ scenario.py
@@ -0,0 +1,16 @@
+# here will be begin initizion
+class Scenario():
+
+    def __init__(self,*args):
+        self.times= times
+        self.angle = angle
+        self.servo_number = servo_number
+        self.port1 ='/dev/ttyACM0'
+        self.port2 = '/dev/ttyUSB0'
+        self.board = pyfirmata.Arduino()
+        self.
+    def up(self):
+        times(self.time)
+        self.servo_number
+
+
--- servo.py
+++ servo.py
@@ -1,15 +1,21 @@
-from multiprocessing import Pool,freeze_support,Process
-import threading
+
 import pyfirmata
 import time
-'''file contain methods for using servo'''
-board = pyfirmata.Arduino('/dev/ttyACM0')
+from main import port2,port
+'file contain methods for using servo'
+board = pyfirmata.Arduino(port2)
 left = board.get_pin('d:8:s')
 right = board.get_pin('d:6:s')
-while True:
-   left.write(360)
-   time.sleep(2)
-   left.write(0)
-   right.write(360)
-   time.sleep(2)
-   right.write(0)
+
+
+def leftw():
+    time.sleep(2)
+    left.write(value)
+    time.sleep(2)
+    left.write(0)
+
+def rightw():
+    right.write(360)
+    time.sleep(20)
+    right.write(0)
+
