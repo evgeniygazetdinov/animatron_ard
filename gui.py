aa0d67bd5be46b2b9c0555fdf674cb053f3ce2c6
evgeshakrasava <zplacesound@gmail.com>
2018-02-19 09:57:16 +0300

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,32 +1,21 @@
 import tkinter
 from tkinter import *
-
-def one(event):
-	print('2')
-def two(event):
-    print('3')
-def three(event):
-    print('1')
+from servo import *
 
 root = Tk()
 btn = Button(root,
              text="first",
              width=30,height=5,
              bg="white",fg="black")
-btn.bind("<Button-1>", one)
+btn.bind("<Button-1>", leftw)
 btn.pack()
 btn2 = Button(root,
              text="second",
              width=30,height=5,
              bg="white",fg="black")
-btn2.bind("<Button-1>", two)
+btn2.bind("<Button-1>", rightw)
 btn2.pack()
-btn3 = Button(root,
-             text="third",
-             width=30,height=5,
-             bg="white",fg="black")
-btn3.bind("<Button-1>", three)
-btn3.pack()
+
 root.mainloop()
 
 
--- servo.py
+++ servo.py
@@ -1,4 +1,36 @@
 
+<<<<<<< HEAD
+
+
+def first_sec():
+    left.write(up)
+    time.sleep(2)
+    left.write(down)
+    time.sleep(2)
+    left.write(up)
+    time.sleep(2)
+    left.write(down)
+
+def second_sec():
+    right.write(up)
+    time.sleep(2)
+    right.write(down)
+    time.sleep(2)
+    right.write(up)
+    time.sleep(2)
+    right.write(down)
+    time.sleep(2)
+    right.write(up)
+def delay10():
+    time.sleep(10)
+def delay():
+    time.sleep(5)
+
+time_bar.append(first_sec)
+time_bar.append(second_sec)
+time_bar.append(delay10)
+delay10(),first_sec(),delay10(),second_sec(),delay10(),delay10()
+=======
 import pyfirmata
 import time
 from main import port2,port
@@ -19,3 +51,4 @@ def rightw():
     time.sleep(20)
     right.write(0)
 
+>>>>>>> 0d26ad640e5622fec04724e44875d02f76bd65c0
