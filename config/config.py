4c1bab9e0a62a89cf93ee5c1ba8249fc7ce0e08c
evgeshakrasava <zplacesound@gmail.com>
2018-02-26 16:07:22 +0300

    Update config.py
    right sequence for servo

--- config/config.py
+++ config/config.py
@@ -6,14 +6,13 @@ port ='/dev/ttyACM0'
 port2 ='/dev/ttyUSB0'
 board = pyfirmata.Arduino(port2)
 #sholder_eft('d:2:s')
-
-sholder_right= board.get_pin('d:7:s')
+right_e = board.get_pin('d:8:s')
+left_e = board.get_pin('d:9:s')
+sholder_right = board.get_pin('d:7:s')
+right_hand = board.get_pin('d:3:s')
+left_hand = board.get_pin('d:6:s')
 left_leg = board.get_pin('d:4:s')
 right_leg = board.get_pin('d:5:s')
-left_hand = board.get_pin('d:6:s')
-right_hand = board .get_pin('d:3:s')
-right_e =board.get_pin('d:8:s')
-left_e = board.get_pin('d:9:s')
 up = 180
 down =90
 
