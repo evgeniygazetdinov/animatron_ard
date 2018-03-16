a899a111b340a389a1ab6988635e4c6c312c6d33
evgeshakrasava <zplacesound@gmail.com>
2018-03-16 12:56:00 +0300

    Add files via upload

---
+++ scenario/servo_checker.py
@@ -0,0 +1,32 @@
+import os
+
+from subprocess import call
+
+
+# call('mkdir sketchbooks',shell =True)
+#change directory
+# os.chdir('/home/qbc/PycharmProjects/ard/saved_/sketchbooks')
+#call('touch readme.txt',shell= True)
+# call('cp -a /usr/share/doc/arduino-mk/examples/Blink .',shell =True)
+os.chdir('/home/qbc/PycharmProjects/ard/saved_/sketchbooks/Blink')
+
+# os.chdir('/home/qbc/PycharmProjects/ard/saved_/sketchbooks/SW/Sweep')
+
+
+call('make',shell=True)
+call("make upload",shell=True)
+# #upload
+# call('make',shell =True)
+# call('make upload',shell =True)
+
+
+
+
+
+
+
+
+
+
+
+
