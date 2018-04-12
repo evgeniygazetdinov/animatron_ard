1c94bf65551fb44bfc1e7066f264dfef274d2a38
evgeshakrasava <zplacesound@gmail.com>
2018-04-12 07:53:09 +0300

    fix speed limit

--- default_positon.py
+++ default_positon.py
@@ -1,5 +1,5 @@
 
-
+# this class discrube default position just only function
 
 
 class Default_position:
@@ -8,11 +8,31 @@ class Default_position:
         pass
 
 
-
-
     def stand_default_position(self,window,value,sec_value):
             if int(window.get()) < int(value) or int(window.get()) > int(sec_value):
                 window.delete(0, 'end')
                 window.insert(0,'{}'.format(value))
                 self.master.after(500,self.stand_default_position(window,value,sec_value))
-        
+
+
+
+
+# in w_v meaning is window variable(all 4 :2window and 2limit for each )
+    def stand_default_loop_position(self,window1,first_w_v1,first_w_v2,):
+        if int(window1.get()) < int(first_w_v1) or int(window1.get()) > int(first_w_v2):
+            window1.set(first_w_v2)
+
+            self.master.after(500,self.stand_default_loop_position(window1,first_w_v1,first_w_v2,))
+
+
+
+
+    def speedlimit_starter(self,window):
+        if int(window.get())  > int(100):
+                window.set(100)
+                self.master.after(500,self.speedlimit_starter(window))
+
+
+
+    def no_more_bigger_interval(self):
+        pass
