63220a045070339df0b333a39a8db0679f1da688
evgeshakrasava <zplacesound@gmail.com>
2018-04-23 19:28:06 +0300

    Add files via upload

--- example2.py
+++ example2.py
@@ -11,20 +11,8 @@ class EXAMPLER:
         self.default_begin = [0,50, 0,50, 100,50, 100,50, 130,50, 30,50, 30,50, 30,50, 0,50]
         self.servo_angles_defaults = [90,50, 90,50, 90,50, 90,50, 90,50, 90,50, 90,50, 90,50, 90,50]
         self.sql_time = 1
-
-
-
-
         self.duration = 18000
 
-
-    def create_values(self):
-        values = {self.time_slider.get():[servo_1.get(),servo_2.get(),
-                                              servo_3.get(),servo_4.get(),
-                                              servo_5.get(),servo_6.get(),
-                                              servo_7.get(),servo_8.get(),
-                                              servo_9.get()]}
-        return values
 
     def time_finder(self):
         # find difference between oldvalues keys and new ,return time execute inter
@@ -35,34 +23,8 @@ class EXAMPLER:
         pass
 
 
-
-
-    def values_on_keys(self):
-        # return all keys to model with interval
-        execute = time_finder()
-
-    def interval_comparer(self):
-        # comprases all interval and return most tiny
-        pass
-
-
-    def inter(self,old_values,new_values):
-        old_values = create_values()
-        first_point = old_values
-        new_values = create_values()
-        second_point = new_values
-        intervals_comprasion(*intervals)
-        keys_creating(min_interval,time)
-        values_on_keys(first_point,second_point,values,keys)
-
-
-
-
-
     def switcher(self,f,s):
         return f,s
-
-
 
 
 
@@ -74,12 +36,7 @@ class EXAMPLER:
         return model
 
 
-    def division_angles(self,min_interval,interval_servo,angle_1,angle_2):
-        # division angle depend on minimal interval
-            division_angle = min_interval/interval_servo
-            first_angle_for_execute  = division_angle/angle_1
-            second_angle_for_execute  = division_angle/angle_2
-            return first_angle_for_execute,second_angle_for_execute
+
 
 
 
@@ -94,38 +51,94 @@ class EXAMPLER:
         return min_interval
 
 
-
-
-    def comprassion(self,time_execute,servo_scale,positon):
-        # servo_scale meaning is scale for comprasion with  default scale in minimal interval
-        # he obtain values from servo scale and changed yourself.if servo angle doesn't have position between
-        for key in servo_scale.keys():
-            # print(time_execute)
-            # print("not changed" +str(time_execute[key][positon]))
-
-            if time_execute[key] >= servo_scale[key]:
-                time_execute[key][positon] = servo_scale[key][positon]
-                # print('changed on'+ str(new_time_execute[key][positon]))
-                # print(new_time_execute[key][positon])
-
     def create_min_keys(self):
         min_interval = self.find_minimal_interval()
         # create time scale with keys  and position
         # time_execute = [self.generateNumber(oldvalues.key,new_values.key,interval)]
-        time_execute = [self.generateNumber(0,15,min_interval,self.default_begin)]
-        print('time_execute is '+str(time_execute))
+        time_execute = self.generateNumber(0,15,min_interval,self.default_begin)
+
         return time_execute
 
 
     def create_some_servo_angles(self):
         servo_angle = self.generateNumber(0,10,5,self.servo_angles_defaults)
-        print('servo execute is '+str(servo_angle))
+
         return servo_angle
+
+
+    def division_angles(self,interval_servo,angle_1,angle_2):
+        # division angle depend on minimal interval
+            min_interval = round(self.find_minimal_interval())
+            division_angle = min_interval/interval_servo
+            first_angle_for_execute  = division_angle/angle_1
+            second_angle_for_execute  = division_angle/angle_2
+            return first_angle_for_execute,second_angle_for_execute
+
+
+
+
+
+    def comprassion(self,positon,number):
+        # servo_scale meaning is scale for comprasion with  default scale in minimal interval
+        # he obtain values from servo scale and changed yourself.if servo angle doesn't have position between
+
+        time_execute = self.create_min_keys()
+        servo_scale = self.create_some_servo_angles()
+        for key in time_execute.keys():
+            if key in servo_scale.keys():
+                # division_angles()
+                time_execute[key][positon] = self.division_angles(4,number,number)
+
+
+                print('time_execute is '+str(time_execute))
+                print('servo_scale is '+str(servo_scale))
+
+
+
+
+
+
+
+
 
     def create_execute(self):
         min_interval = self.create_min_keys()
         servo_angle = self.create_some_servo_angles()
-        self.comprassion(min_interval,servo_angle,1)
+        execute = self.comprassion(0,99)
+        execute = self.comprassion(2,100)
+        execute = self.comprassion(4,120)
+        execute = self.comprassion(6,130)
+        execute = self.comprassion(8,190)
+        execute = self.comprassion(10,220)
+        execute = self.comprassion(12,320)
+        execute = self.comprassion(14,50)
+        execute = self.comprassion(16,999)
+        print(execute)
+        return execute
+
+    def use_execute(self):
+        execute = create_execute()
+        for key,value in execute.items():
+            self.sql_speed1 = value[1]
+            self.sql_speed2 = value[3]
+            self.sql_speed3 = value[5]
+            self.sql_speed4 = value[7]
+            self.sql_speed5 = value[9]
+            self.sql_speed6 = value[11]
+            self.sql_speed7 = value[13]
+            self.sql_speed8 = value[15]
+            self.sql_speed9 = value[17]
+            self.sql_servo_1 = value[0]
+            self.sql_servo_2 = value[0]
+            self.sql_servo_3 = value[0]
+            self.sql_servo_4 = value[0]
+            self.sql_servo_5 = value[0]
+            self.sql_servo_6 = value[0]
+            self.sql_servo_7 = value[0]
+            self.sql_servo_8 = value[0]
+            self.sql_servo_9 = value[0]
+
+
 
 
 
