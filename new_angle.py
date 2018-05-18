8d8a3e8ab693e48444cf981ad60dd3ad802ca534
evgesha <zplacesound@gmail.com>
2018-05-18 20:37:41 +0300

    some angle shit

---
+++ new_angle.py
@@ -0,0 +1,39 @@
+def find_bigger(begin_div,over_div):
+    duplicate_begin = begin_div
+    duplicate_over = over_div
+    if begin_div < over_div:
+        begin_div = duplicate_over
+        over_div = duplicate_begin
+        return begin_div
+    else:
+        return begin_div
+def overnd_interval(interval):
+    if interval %2 != 0:
+        interval+1
+        return interval
+    else:
+        return interval
+
+
+def new_angle(min_interval,interval_servo,begin_div,over_div,key):
+    intervals_servo  = overnd_interval(interval_servo)
+    interval_div =round(interval_servo )/round(min_interval)
+    bigger_angle_for_div = find_bigger(begin_div,over_div)
+    angle_div = round(int(bigger_angle_for_div)) / round(int(interval_div))
+    safe = round(angle_div)
+    execute = [round(angle_div)]
+    for i in range(key):
+        angle_div += safe
+        if angle_div >= over_div:
+            angle_div-= safe
+            safe*= -1
+        if angle_div <= 0:
+            angle_div += angle_div
+            safe*= -1
+        execute.append(round(angle_div))
+    print(len(execute))
+    return execute
+
+
+
+print(new_angle(1,5,2,180,40))
