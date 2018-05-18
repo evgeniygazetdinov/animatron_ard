0e29580674af8374bde7b6bd90403643bc5ceb88
evgesha <zplacesound@gmail.com>
2018-05-18 20:44:30 +0300

    Add files via upload

--- new_angle.py
+++ new_angle.py
@@ -9,7 +9,8 @@ def find_bigger(begin_div,over_div):
         return begin_div
 def overnd_interval(interval):
     if interval %2 != 0:
-        interval+1
+        interval+=1
+        print(interval)
         return interval
     else:
         return interval
@@ -17,14 +18,14 @@ def overnd_interval(interval):
 
 def new_angle(min_interval,interval_servo,begin_div,over_div,key):
     intervals_servo  = overnd_interval(interval_servo)
-    interval_div =round(interval_servo )/round(min_interval)
+    interval_div =round(intervals_servo )/round(min_interval)
     bigger_angle_for_div = find_bigger(begin_div,over_div)
     angle_div = round(int(bigger_angle_for_div)) / round(int(interval_div))
     safe = round(angle_div)
     execute = [round(angle_div)]
     for i in range(key):
         angle_div += safe
-        if angle_div >= over_div:
+        if angle_div == over_div:
             angle_div-= safe
             safe*= -1
         if angle_div <= 0:
@@ -36,4 +37,5 @@ def new_angle(min_interval,interval_servo,begin_div,over_div,key):
 
 
 
+print(new_angle(1,5,2,180,40))
 print(new_angle(1,5,2,180,40))
